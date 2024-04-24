455d4eba58a565cc2bcb5d22408e027c2f1741fc
Tanya206 <92422597+Tanya206@users.noreply.github.com>
2024-04-24 23:37:53 +0530

    hihihihahahaha

--- src/ethereum/contracts/Minor.sol
+++ src/ethereum/contracts/Minor.sol
@@ -1,73 +1,59 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.9;
- 
-contract Minor {
-    address public admin;
-    struct member {
-        uint id;
-        string name;
-        uint adhaar;
-        uint age;
-        uint star;
-        address add;
-    }
-    //star==0 judge, 1 lawyer, 2 student and others // -1 admin
 
-    member [] public members;
-    constructor() {
-        admin = msg.sender;
-        member storage newMember= members.push();
-        uint memberId= members.length;
-        
-        newMember.name= "Admin";
-        newMember.adhaar=12345678912;
-        newMember.age=25;
-        newMember.star=3;    
-        newMember.id= 1;   
-        newMember.add= admin;
-        id[admin]= memberId;
-    }
-    mapping ( address=> uint ) public id;
-
-    function insert(string memory name, uint adhaar, uint age, uint star, address add) public {
-        require(msg.sender==admin);
-        // id[msg.sender]=0;
-        
-        member storage newMember= members.push();
-        uint memberId= members.length;
-        
-        newMember.name= name;
-        newMember.adhaar=adhaar;
-        newMember.age=age;
-        newMember.star=star;    
-        newMember.id= memberId;   
-        newMember.add= add;
-        id[add]= memberId;
-    }
-}   
-
-contract File{
-    struct fileDetails{
-        string ipfsHash;
-        uint uploadTime;
-        address uploader;
-        bool exists;
-    }
-    mapping(string => fileDetails)fileMap;
+contract CaseFactory{
+    address [] addressArray ;      
+    mapping (address => address []) cases;          //lawyer address=>array of all case addresses
+    uint public caseCnt;
     
-    //event FileUploaded(string addr, uint dateTime, address indexed )
-
-    function upload(string memory _ipfsHash) public {//view returns(fileDetails memory){
+    function createCase( address clientAddress , uint dateOfFiling) public{
+        caseCnt++;
+        address newCase = address(new Case(caseCnt, msg.sender,  clientAddress,   dateOfFiling));
+        cases[msg.sender].push(newCase);   
+        cases[clientAddress].push(newCase);     //new case is the address of the case contract 
         
-        require(bytes(_ipfsHash).length > 0, "IPFS hash cannot be empty");
-          
-        fileMap[_ipfsHash] = fileDetails({
-            ipfsHash: _ipfsHash,
-            uploadTime: block.timestamp,
-            uploader: msg.sender,
-            exists: true        
-        });
     }
+    function returnAddress(address addr) public view returns(address [] memory) {
+        return (cases[addr]);
+    } 
 }
 
+contract Case{
+    uint caseid;
+    uint dof;
+    address lawyer;
+    address client;
+    constructor (uint caseID, address lawyerAddress, address clientAddress, uint dateOfFiling){
+        lawyer = lawyerAddress;
+        client = clientAddress;
+        dof = dateOfFiling;
+        caseID = caseid;
+        
+    }
+     struct fileDetails{
+        address addr;
+        uint dateTime;
+        address addrUpload;
+        bool exist;
+    }
+    mapping(address => fileDetails)fileMap;//string ke jgh cid aayega yaani ki address
+    //array of address;
+    address [] IPFSAdd;
+    function insertAddr(address add) public{
+        IPFSAdd.push(add);
+    }
+    //function to upload files in array
+    function fetchAddr() public view returns(address[] memory){
+        return IPFSAdd;
+    }
+    function upload(address addr) public view{
+        fileDetails memory file; 
+        file.addr= addr;
+        file.dateTime= block.timestamp;
+        file.addrUpload= msg.sender;
+        file.exist= true;
+    }
+
+
+}
